219d1375a3a9532edade975933cd9c5c2fbdd292
marcinlen <akratic120@gmail.com>
2018-01-26 13:37:25 +0000

    only code necessary for program to run

--- classic_greetings/classic_greetings/Program.cs
+++ classic_greetings/classic_greetings/Program.cs
@@ -1,25 +1,19 @@
 ﻿/*
  * Program that prints greeting to the console.
  * 
- * M Lentner 2018
+ * M Lentner Jan 2018
  */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace classic_greetings
+class Program
 {
-    class Program
+    private static void Main()
     {
-        private static void Main()
-        {
-            Console.WriteLine("Hello, world!");
+        Console.WriteLine("Hello, world!");
 
-            // prevent console window from closing 
-            Console.ReadLine();
-        }
+        // prevent console window from closing 
+        Console.ReadLine();
     }
 }
+
