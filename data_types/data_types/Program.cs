550e9b52a723b1b9a3873fea8ce0970aca7a4f77
marcinlen <akratic120@gmail.com>
2018-01-27 11:27:29 +0000

    added integer types

--- data_types/data_types/Program.cs
+++ data_types/data_types/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿/*
+ * Presents data types of C# language.
+ * Template: C# Console App .NET Core
+ * 
+ * Marcin Lentner Jan 2018
+ */
+
+using System;
 
 namespace data_types
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int int_standard = 3;
+            System.Int32 int_net = 3;
+
+            Console.WriteLine("Standard integer type = {0}, .NET integer type = {1}", int_standard, int_net);
+
+            // wait for user input to close the window
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
         }
     }
 }
