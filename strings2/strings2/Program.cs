8d475ba218fe44bbe557f73397f6d3fa7e84c21a
marcinlen <akratic120@gmail.com>
2018-03-08 17:17:01 +0000

    added new solution

---
+++ strings2/strings2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace strings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
