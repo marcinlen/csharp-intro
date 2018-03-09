65dfaa289fac047f51e14c87ec449d408b7848b1
marcinlen <akratic120@gmail.com>
2018-03-09 18:22:21 +0000

    calling methods on strings

--- string_operations/string_operations/Program.cs
+++ string_operations/string_operations/Program.cs
@@ -8,8 +8,14 @@ namespace string_operations
         {
             string city = "Leeds";
             String country = "United Kingdom";
+            string countryCode = "uk";
 
+            // call methods on objects
+            countryCode = countryCode.ToUpper();
+
+            // string formatting
             Console.WriteLine("Welcome to {0}, {1}.", city, country);
+            Console.WriteLine("Welcome to {0}.", countryCode);
 
             Console.ReadKey();
         }
